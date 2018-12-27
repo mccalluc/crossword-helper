09d2630a7ece17adbfb050d985c9d9a318a39c58
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-12-26 23:26:41 -0500

    machinery

---
+++ best_word.py
@@ -0,0 +1,6 @@
+def best_word(sequence, words, on_top=True):
+    '''
+    Given a sequence of characters, and a list of words,
+    returns the word and an offset which produces the best bigrams.
+    '''
+    pass
