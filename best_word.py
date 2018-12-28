2daf5b5b97b0e8e8b73589a55c968c0915c83872
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-12-28 01:13:16 -0500

    flake8

--- best_word.py
+++ best_word.py
@@ -1,17 +1,18 @@
 from collections import defaultdict
 from math import log2
 
+
 def best_word(sequence, words, on_top=True):
     '''
     Given a sequence of characters, and a list of words,
     and a flag which indicates whether the word we seek is above
     or below the sequence,
     returns the word and an offset which produces the best bigrams.
-    
+
     >>> words = {'boy', 'aka', 'abba', 'book', 'year', 'tab'}
     >>> best_word('oboe', words)
     ('abba', 2)
-    
+
     >>> best_word('oboe', words, on_top=False)
     ('book', 1)
     '''
@@ -32,13 +33,14 @@ def best_word(sequence, words, on_top=True):
                 best_word = word
                 best_offset = offset
     return (best_word, best_offset)
-            
+
+
 def score(pair_bigrams, all_bigrams):
     '''
     Returns an aggregate score for a set of bigrams,
     against a defaultdict of all bigrams.
     Bigrams with spaces are ignored.
-    
+
     >>> all_bigrams = defaultdict(int, {'ab': 1, 'cd': 4})
     >>> score(['ab', 'a ', ' a'], all_bigrams)
     0.0
@@ -50,10 +52,11 @@ def score(pair_bigrams, all_bigrams):
     counts = [all_bigrams[bg] for bg in pair_bigrams if ' ' not in bg]
     return combine_counts(counts)
 
+
 def combine_counts(counts):
     '''
     Given a set of counts, returns an aggregate score.
-    
+
     >>> combine_counts([2,0])
     -inf
     >>> combine_counts([2,2])
@@ -64,25 +67,27 @@ def combine_counts(counts):
     if 0 in counts:
         return -float('inf')
     logs = [log2(count) for count in counts]
-    return sum(logs)    
-            
+    return sum(logs)
+
+
 def alignment_bigrams(a_str, b_str):
     '''
     Given two strings of equal length,
     return the bigrams of their alignment.
-    
+
     >>> alignment_bigrams('abc', 'xyz')
     ['ax', 'by', 'cz']
     '''
     return [a + b for a, b in zip(a_str, b_str)]
 
+
 def offset_overlaps(sequence, word):
     '''
     Return offsets and overlaps of word against sequence.
-    
+
     >>> offset_overlaps('abc','xy')
     [(-2, '  x'), (-1, ' xy'), (0, 'xy '), (1, 'y  ')]
-    
+
     >>> offset_overlaps('a','xyz')
     [(0, 'x'), (1, 'y'), (2, 'z')]
     '''
@@ -90,17 +95,18 @@ def offset_overlaps(sequence, word):
         (i - len(sequence) + 1, overlap)
         for i, overlap in enumerate(overlaps(sequence, word))
     ]
-            
+
+
 def overlaps(sequence, word):
     '''
     Returns all the possible alignments of word against sequence.
-    
+
     >>> overlaps('hat', 'cat')
     ['  c', ' ca', 'cat', 'at ', 't  ']
-    
+
     >>> overlaps('x', 'abcdefg')
     ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-    
+
     >>> overlaps('abc', 'x')
     ['  x', ' x ', 'x  ']
     '''
@@ -110,11 +116,12 @@ def overlaps(sequence, word):
         padded_word[offset:offset + len(sequence)]
         for offset in range(len(word) + len(sequence) - 1)
     ]
-    
+
+
 def bigrams(words):
     '''
     Returns a dict of bigram counts in the set of words.
-    
+
     >>> bg = bigrams(['cat', 'mat'])
     >>> bg['ca']
     1
